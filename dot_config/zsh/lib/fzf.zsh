5afce4e21a9b3d424c7c497f9ba8fbe7b609dac3
Stephan Komp <komp.stephan@gmail.com>
2020-12-13 19:26:26 +0100

    Add variables

--- dot_config/zsh/lib/fzf.zsh
+++
@@ -1,5 +0,0 @@
-if [[ -e /usr/share/fzf ]]; then
-  source '/usr/share/fzf/completion.zsh'
-  source '/usr/share/fzf/key-bindings.zsh'
-  export FZF_DEFAULT_COMMAND='fd ""'
-fi
--- run_once_install-packages.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ ! -e $HOME/.config/zsh/zgen ]]; then
-  git clone https://github.com/tarjoilija/zgen.git "$HOME/.config/zsh/zgen"
-fi
